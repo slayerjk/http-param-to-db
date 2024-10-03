9580380faf04b3b599309ffed56ee2be3dd3fc2b
slayerjk <slayer.jk@mail.ru>
2024-10-03 16:29:22 +0500

    main

--- cmd/api-to-db.go
+++ cmd/api-to-db.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"log"
+	"net/http"
 	"time"
 
 	// change this path for your project
@@ -15,6 +16,34 @@ const (
 	defaultLogPath    = "logs"
 	defaultLogsToKeep = 3
 )
+
+// http handlers
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("HELLO!"))
+}
+
+// extract query parameter
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		log.Printf("Got query: %v", r.URL.String())
+
+		if r.URL.Query().Has("name") {
+			log.Printf("Name: %s", r.URL.Query().Get("name"))
+			w.Write([]byte("OK"))
+			return
+		}
+
+		w.Write([]byte("No `name` parameter!\n"))
+	}
+
+	w.Write([]byte("Only POST allowed!\n"))
+	log.Printf("wrong parameter in POST: %v\n", r.URL.String())
+}
+
+func registerHanlers() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/body", postHandler)
+}
 
 func main() {
 	// flags
@@ -37,6 +66,14 @@ func main() {
 	log.Println("Program Started")
 
 	// main code here
+
+	// starting web server
+	mux := http.DefaultServeMux
+	registerHanlers()
+	errMux := http.ListenAndServe(":3000", mux)
+	if err != nil {
+		log.Fatalf("failed to run server:\n\t%v", errMux)
+	}
 
 	// count & print estimated time
 	endTime := time.Now()
