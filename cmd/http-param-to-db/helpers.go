d9abe0ae9626c78b674a607615eccd8e4a5c9b93
slayerjk <slayer.jk@mail.ru>
2025-02-25 15:39:41 +0500

    + added security headers(middleware); * refactored http errors(helpers.go)

---
+++ cmd/http-param-to-db/helpers.go
@@ -0,0 +1,28 @@
+package main
+
+import "net/http"
+
+// http.Error(w, errParamNo, http.StatusBadRequest)
+// http.Error(w, "db error", http.StatusInternalServerError)
+
+// return err to logger.Warn and text to response writer(StatusBadRequest)
+func (app *application) badRequestError(w http.ResponseWriter, r *http.Request, loggerError string) {
+	var (
+		method = r.Method
+		uri    = r.URL.RequestURI()
+	)
+
+	app.logger.Warn(loggerError, "METHOD", method, "URI", uri)
+	http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+}
+
+// return err to logger.Warn and text to response writer(StatusBadRequest)
+func (app *application) serverError(w http.ResponseWriter, r *http.Request, loggerError string) {
+	var (
+		method = r.Method
+		uri    = r.URL.RequestURI()
+	)
+
+	app.logger.Error(loggerError, "METHOD", method, "URI", uri)
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
